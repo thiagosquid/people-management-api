e8491bb64a315bd06f430ff92288d840748e8a52
Thiago Gomes <thiagofg.eng@gmail.com>
2021-10-16 14:52:30 -0300

    implements get by id and delete endpoints

--- src/main/java/com/thiago/personapi/controllers/PersonController.java
+++ src/main/java/com/thiago/personapi/controllers/PersonController.java
@@ -4,6 +4,7 @@ package com.thiago.personapi.controllers;
 import com.thiago.personapi.dto.request.PersonDTO;
 import com.thiago.personapi.dto.response.MessageResponseDTO;
 import com.thiago.personapi.entities.Person;
+import com.thiago.personapi.exceptions.PersonNotFoundException;
 import com.thiago.personapi.repositories.PersonRepository;
 import com.thiago.personapi.services.PersonService;
 import lombok.AllArgsConstructor;
@@ -31,6 +32,18 @@ public class PersonController {
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
 
         return personService.create(personDTO);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePerson(@PathVariable Long id) throws PersonNotFoundException {
+        personService.deleteById(id);
     }
 
 
--- src/main/java/com/thiago/personapi/services/PersonService.java
+++ src/main/java/com/thiago/personapi/services/PersonService.java
@@ -4,6 +4,7 @@ import com.thiago.personapi.dto.mapper.PersonMapper;
 import com.thiago.personapi.dto.request.PersonDTO;
 import com.thiago.personapi.dto.response.MessageResponseDTO;
 import com.thiago.personapi.entities.Person;
+import com.thiago.personapi.exceptions.PersonNotFoundException;
 import com.thiago.personapi.repositories.PersonRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,8 +39,26 @@ public class PersonService {
         return messageResponseDTO;
     }
 
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = verifyIfExists(id);
+
+        return personMapper.toDTO(person);
+
+    }
+
+    public void deleteById(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+
+        return personRepository.findById(id).orElseThrow(()-> new PersonNotFoundException(id));
+
+    }
+
     private MessageResponseDTO createMessageResponse(String message, Long id){
         return MessageResponseDTO.builder().message(message + id).build();
     }
-
 }
