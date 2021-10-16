46e60ffe8eff50aa02e7ceacb564808bd8e684dc
Thiago Gomes <thiagofg.eng@gmail.com>
2021-10-16 12:54:02 -0300

    created PhoneDTO class

--- src/main/java/com/thiago/personapi/dto/request/PhoneDTO.java
+++ src/main/java/com/thiago/personapi/dto/request/PhoneDTO.java
@@ -1,4 +1,29 @@
 package com.thiago.personapi.dto.request;
 
+import com.thiago.personapi.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class PhoneDTO {
+
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PhoneType type;
+
+    @NotEmpty
+    @Size(min=12, max=14)
+    private String number;
+
 }
